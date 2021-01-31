7d5ada86705b417fbcb51d6e1a75b218b0b51931
shirinstar <shirinanlen@gmail.com>
2021-01-30 20:35:58 -0800

    relation items

--- data/listoflist.py
+++ data/listoflist.py
@@ -16,9 +16,8 @@ def gen_subsets(rel):
   return sets
 
 def build_counts(): 
-  f = open("/Users/shirin/other_projects/cooper_hewitt/Pen_Data_Dictionary/listoflist.txt")
+  f = open("/Users/shirin/other_projects/cooper_hewitt/Pen_Data_Dictionary/Pen_Data_Dictionary/data/visitsItems_listevening.txt", "r")
   data = json.loads(f.read())
-
   relation_counts = {}
   ctr = 0
   vi_length = len(data)
@@ -26,17 +25,20 @@ def build_counts():
     ctr += 1
     print("on visit item: #" + str(ctr) + " out of total items: #" + str(vi_length))
     subsets = gen_subsets(visit_items)
+
     print("total subsets length: " + str(len(subsets)))
     for rel in subsets:
       relation_counts[rel] = relation_counts.get(rel, 0) + 1
 
+
+  import pdb; pdb.set_trace()
 
   print("before filter")
   relation_counts = dict(filter(lambda elem: elem[1] > 1, relation_counts.items()))
   print("before sorting")
   answer = dict(sorted(relation_counts.items(), key=lambda item: item[1]))
   print("before pickling")
-  f = open("/Users/shirin/other_projects/cooper_hewitt/Pen_Data_Dictionary/relation_counts.json", "wb")
+  f = open("/Users/shirin/other_projects/cooper_hewitt/Pen_Data_Dictionary/relation_counts_eveningTime.json", "wb")
   pickle.dump(answer, f)
   f.close()
   import pdb; pdb.set_trace()
@@ -46,6 +48,5 @@ def get_counts():
   f = open("/Users/shirin/relation_counts.json", "rb")
   data = pickle.load(f)
   f.close()
-  import pdb; pdb.set_trace()
 
-get_counts()
+build_counts()
